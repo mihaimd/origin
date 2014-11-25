0acfb5af0bf44567dc6193df3a37169dbad9b9e0
mmicsa <mmicsa@pentalog.fr>
2014-11-25 09:35:50 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+class Cucu {
+
+	$var= 'In physics, energy is a property of objects, transferable among them via fundamental interactions, which can be converted in form but not created or destroyed.';
+
+	function Breakdown($var){
+	
+	$var2= array();
+
+	$var2= explode(' ', $var);
+	
+	var_dump($var2);
+
+	}
+
+}
